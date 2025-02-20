4af4b0559d6ef75b3065f02e61128ae2be71b7b9
Aaryan-Biradar <aaryan.biradar@outlook.com>
2025-02-20 10:59:41 -0500

    created first 4 functions

--- subsys.c
+++ subsys.c
@@ -5,3 +5,18 @@
     This file should contain the implementations for Subsystem specific functionality.
     Initialization, printing, setting and getting data and statuses.
 */
+
+int init_subsystem(Subsystem *subsystem, const char *name, char status) {
+    if (subsystem == NULL || name == NULL || status == NULL) {
+        return ERR_NULL_POINTER;
+    }
+
+    strncpy(subsystem->name, name, MAX_STR - 1);
+    subsystem->name[MAX_STR - 1] = '\0';
+    subsystem->status = status;
+    subsystem->data = 0;
+
+    return ERR_SUCCESS;
+}
+
+
--- subsys_collection.c
+++ subsys_collection.c
@@ -5,3 +5,43 @@
     This file should contain your documentation and implementations for SubsystemCollection 
     specific functionality; initialization, appending, removal, printing, finding, and filtering.
 */
+
+int subsys_collection_init(SubsystemCollection *subsystems) {
+    if (subsystems == NULL) {
+        return ERR_NULL_POINTER;
+    }
+
+    subsystems->size = 0;
+
+    return ERR_SUCCESS;
+}
+
+
+int subsys_append(SubsystemCollection *subsystems, const Subsystem *subsystem) {
+    if (subsystems == NULL || subsystem == NULL) {
+        return ERR_NULL_POINTER;
+    }
+
+    if (subsystems->size >= MAX_ARR) {
+        return ERR_MAX_CAPACITY;
+    }
+
+    subsystems->subsystems[subsystems->size] = *subsystem;
+    subsystems->size++;
+
+}
+
+int subsys_find(const SubsystemCollection *subsystems, const char *name) {
+    if (subsystems == NULL || name == NULL) {
+        return ERR_NULL_POINTER;
+    }
+
+    for (int i = 0; i < subsystems->size; i++) {
+        if (strcmp(subsystems->subsystems[i].name, name) == 0) {
+            return i;
+        }
+    }
+
+    return ERR_SYS_NOT_FOUND;
+}
+
